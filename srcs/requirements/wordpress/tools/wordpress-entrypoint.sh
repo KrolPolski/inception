3d31c790a414c52e1872bf6e30e040eaf8d1e748
Ryan Boudwin <ryanboudwin@outlook.com>
2025-04-22 22:06:19 +0300

    Three containers running but still giving 502 errors

---
+++ srcs/requirements/wordpress/tools/wordpress-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+# Wait for database
+until mysql -h$WORDPRESS_DB_HOST -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -e "SELECT 1"; do
+    echo "Waiting for MariaDB..."
+    sleep 3
+done
+
+# WordPress installation
+if [ ! -f wp-config.php ]; then
+   php -d memory_limit=512M /usr/local/bin/wp core download --allow-root
+
+    # Configure WordPress
+    wp config create \
+        --dbname=$WORDPRESS_DB_NAME \
+        --dbuser=$WORDPRESS_DB_USER \
+        --dbpass=$WORDPRESS_DB_PASSWORD \
+        --dbhost=$WORDPRESS_DB_HOST \
+        --allow-root
+
+    # Install WordPress
+    wp core install \
+        --url=$WORDPRESS_URL \
+        --title=$WORDPRESS_TITLE \
+        --admin_user=$WORDPRESS_ADMIN_USER \
+        --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+        --admin_email=$WORDPRESS_ADMIN_EMAIL \
+        --allow-root
+fi
+
+exec "$@"
