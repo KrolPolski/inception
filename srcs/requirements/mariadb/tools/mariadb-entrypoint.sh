9e71489e4f88d5dfc0deb029ccce810729b58c39
Ryan Boudwin <rboudwin@student.hive.fi>
2025-04-24 11:42:28 +0300

    Mariadb container appears to be working more or less

--- srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb-entrypoint.sh
@@ -1,39 +1,26 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
 
-# The first time we run this, configure the server to be reachable
-# from other containers
-if [ ! -e /etc/.firstrun ]; then
-	cat << EOF >> /etc/my.cnf.d/mariadb-server.cnf
-[mysqld]
-bind-address=0.0.0.0
-skip-networking=0
-EOF
-	touch /etc/.firstrun
-fi
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing MariaDB data directory..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-# On the first volume mount, create a database in it
-if [ ! -e /var/lib/mysql/.firstmount ]; then
-	# Initialize a database on the volume and start MariaDB in the background
-	mysql_install_db --datadir=/var/lib/mysql --skip-test-db --user=mysql --group=mysql \
-		--auth-root-authentication-method=socket >/dev/null 2>/dev/null
-	mysqld_safe &
-	mysqld_pid=$!
+    mysqld --user=mysql --bootstrap << EOF
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 
-	# Wait for the server to be started, then set up database and accounts
-	mysqladmin ping -u root --silent --wait >/dev/null 2>/dev/null
-	cat << EOF | mysql --protocol=socket -u root -p=
-CREATE DATABASE $MYSQL_DATABASE;
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-GRANT ALL PRIVILEGES ON *.* to 'root'@'%' IDENTIFIED BY '$MYSQLROOT_PASSWORD';
+
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
 EOF
-
-	# Shut downt he temporary server and mark the volume as initialized
-	mysqladmin shutdown
-	touch /var/lib/mysql/.firstmount
 fi
 
-exec mysqld_safe
+echo "Starting MariaDB server..."
+/usr/bin/mariadbd-safe --datadir='/var/lib/mysql'
+#exec "$@"
